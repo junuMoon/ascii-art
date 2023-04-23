ab4780ecb75486ebfdf80e8832f9162f08259b41
Fran Moon <francomoon7@gmail.com>
2023-04-23 22:45:34 +0900

    drew rhombus first
    todo: refactor it with nested for loop to represent space

--- src/doughnut.py
+++ src/doughnut.py
@@ -1,7 +1,18 @@
-import os
+def draw_rhombus(character, width):
+    c = character
+    mid = width // 2
+    for i in range(mid):
+        if not i:
+            print("{:>{}}".format(c, mid))
+            continue
+        print("{:>{}}".format(c, mid-i), end='')
+        print("{:>{}}".format(c, i*2))
+    for i in range(2, mid):
+        print("{:>{}}".format(c, i), end='')
+        print("{:>{}}".format(c, width-(i*2)))
+    print("{:>{}}".format(c, mid))
 
-def draw_circle(chracter):
-    print('O')
 
 if __name__ == '__main__':
-    draw_circle('@')
+    draw_rhombus('@', 10)
+
